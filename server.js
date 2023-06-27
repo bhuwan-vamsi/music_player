29fc97e21353f886a8bbdacc3bff04cc07dc1c7d
immahmoodpasha <119700375+immahmoodpasha@users.noreply.github.com>
2023-06-27 18:08:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,16 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect('mongodb://127.0.0.1:27017/musicdetails', {
+//connect
+const path=require("path");
+app.use(express.static(path.join(__dirname,'./build')))
+//refresh
+const pageRefresh=(request,response,next)=>{
+  response.sendFile(path.join(__dirname,'./build/index.html'))
+}
+app.use("/*",pageRefresh);
+
+mongoose.connect('mongodb://127.0.0.1:27017/vnrdb', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
